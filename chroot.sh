cf01b299ea4221f61f4b15269cca0454d06485b9
Leonardo Monteiro <leo.monteiro06@live.com>
2023-07-20 11:22:47 -0300

    Update chroot.sh

--- chroot.sh
+++ chroot.sh
@@ -15,11 +15,12 @@ ROOT_UUID=$(blkid -s UUID -o value /dev/mapper/cryptroot)
 printf "UUID=$ROOT_UUID / btrfs $BTRFS_OPTS,subvol=@ 0 1\nUUID=$UEFI_UUID /efi vfat ro,defaults,noatime 0 2\nUUID=$GRUB_UUID /boot ext2 defaults,noatime 0 2\nUUID=$ROOT_UUID /home btrfs $BTRFS_OPTS,subvol=@home 0 2\nUUID=$ROOT_UUID /.snapshots btrfs $BTRFS_OPTS,subvol=@snapshots 0 2\n/var/swap/swapfile none swap sw 0 0\n" >> /etc/fstab
 echo hostonly=yes >> /etc/dracut.conf
 
+xbps-install -Su -y void-repo-nonfree void-repo-multilib
+
 mkdir -p /etc/xbps.d
 cp /usr/share/xbps.d/*-repository-*.conf /etc/xbps.d/
-sed -i 's|https://repo-default.voidlinux.org|https://voidlinux.com.br/repo/|g' /etc/xbps.d/*-repository-*.conf
+sed -i 's|https://repo-default.voidlinux.org|https://repo-fastly.voidlinux.org|g' /etc/xbps.d/*-repository-*.conf
 
-xbps-install -Su -y void-repo-nonfree void-repo-multilib
 xbps-install -S -y intel-ucode grub-x86_64-efi
 
 grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id="Void Linux"
